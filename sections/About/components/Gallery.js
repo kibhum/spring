edebe471045b0c037c2a88dc59845f7adbd80565
kibethh <kibethh@gmail.com>
2024-03-26 02:55:13 +0300

    Changed title

--- sections/About/components/Gallery.js
+++ sections/About/components/Gallery.js
@@ -3,26 +3,29 @@ import React from "react";
 import Slider from "react-slick";
 import ImageCard from "./Images";
 import { getImages } from "../content/fetcher";
+import Box from "@mui/material/Box";
 const images = getImages();
 
 export default function Gallery() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 200,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
   return (
-    <Slider {...settings}>
-      {images.map((image) => (
-        <ImageCard
-          key={image.title}
-          title={image.title}
-          path={image.path}
-          description={image.description}
-        />
-      ))}
-    </Slider>
+    <Box sx={{ width: "100%" }}>
+      <Slider {...settings}>
+        {images.map((image) => (
+          <ImageCard
+            key={image.title}
+            title={image.title}
+            path={image.path}
+            description={image.description}
+          />
+        ))}
+      </Slider>
+    </Box>
   );
 }
